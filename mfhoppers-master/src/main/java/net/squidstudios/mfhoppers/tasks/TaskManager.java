bae5df5b02500626db886d4b9d8389a05429cb80
Tobias Adloff <tadloff@mail.upb.de>
2020-01-19 18:39:45 +0100

    Fix 1.15 problem with getDrops

--- mfhoppers-master/src/main/java/net/squidstudios/mfhoppers/tasks/TaskManager.java
+++ mfhoppers-master/src/main/java/net/squidstudios/mfhoppers/tasks/TaskManager.java
@@ -332,6 +332,8 @@ public class TaskManager implements Listener {
     public void runBreakTask() {
         final Set<IHopper> hoppers = Methods.getActiveHopperByType(HopperEnum.Break);
 
+        ItemStack tool = new ItemStack(Material.DIAMOND_PICKAXE);
+
         for (IHopper hopper : hoppers) {
             if (!hopper.isChunkLoaded()) {
                 continue;
@@ -358,9 +360,9 @@ public class TaskManager implements Listener {
 
                 final List<ItemStack> dropItems = new LinkedList<>();
                 if (!dropElement.HasDamageValue) {
-                    upper.getBlock().getDrops().forEach(it -> dropItems.add(dropElement.Drop.getItem(it.getType())));
+                    upper.getBlock().getDrops(tool).forEach(it -> dropItems.add(dropElement.Drop.getItem(it.getType())));
                 } else {
-                    upper.getBlock().getDrops().forEach(it -> {
+                    upper.getBlock().getDrops(tool).forEach(it -> {
 
                         ItemStack item = dropElement.Drop.getItem(it.getType());
                         item.setDurability(dropElement.DamageValue);
