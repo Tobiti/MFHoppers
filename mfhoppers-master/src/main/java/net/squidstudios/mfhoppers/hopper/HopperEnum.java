c0234cddb3302ebf2937133db2c8704b117c3935
Tobias Adloff <tadloff@mail.upb.de>
2020-03-28 18:50:31 +0100

    Add Hopper type normal

--- mfhoppers-master/src/main/java/net/squidstudios/mfhoppers/hopper/HopperEnum.java
+++ mfhoppers-master/src/main/java/net/squidstudios/mfhoppers/hopper/HopperEnum.java
@@ -4,7 +4,8 @@ public enum HopperEnum {
     Break(new String[]{"type", "limitPerChunk", "name", "lore", "should_drop_from_explosions", "breakEvery", "drops"}, new String[]{"limitPerChunk", "lore", "name", "breakEvery","drops"}),
     Grind(new String[]{"type", "limitPerChunk", "name", "lore", "should_drop_from_explosions", "mob", "damage", "time"},new String[]{"limitPerChunk", "lore", "name", "defaultMob", "damage", "time"}),
     Crop(new String[]{"type", "limitPerChunk", "name", "lore", "should_drop_from_explosions", "crops"}, new String[]{"limitPerChunk", "lore", "name", "defaultMob", "crops"}),
-    Mob(new String[]{"type", "limitPerChunk", "name", "lore", "should_drop_from_explosions", "drops"}, new String[]{"limitPerChunk", "lore", "name", "defaultMob", "drops"});
+    Mob(new String[]{"type", "limitPerChunk", "name", "lore", "should_drop_from_explosions", "drops"}, new String[]{"limitPerChunk", "lore", "name", "defaultMob", "drops"}),
+    Normal(new String[]{"type", "limitPerChunk", "name", "lore", "should_drop_from_explosions"}, new String[]{"limitPerChunk", "lore", "name"});;
     private String[] req = {""};
     private String[] upgrades = {""};
     HopperEnum(String[] req){
